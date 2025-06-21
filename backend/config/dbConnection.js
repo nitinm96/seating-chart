8a10b08ab06fa1c94ea87c0b62b0f9e04b288d61
Nitin Minhas <nitin.minhas@hotmail.com>
2025-06-21 14:01:24 -0400

    fixing env variable names

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -3,10 +3,10 @@ const dotenv = require("dotenv").config();
 
 //database connection
 const db = mysql2.createConnection({
-  host: process.env.HOST,
-  user: 'root', //process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.MYSQLHOST,
+  user: "root", //process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
-console.log(`Connected to database: ${process.env.DATABASE}`);
+console.log(`Connected to database: ${process.env.MYSQL_DATABASE}`);
 module.exports = { db };
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const cors = require("cors"); 
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 const app = express();
-const PORT = process.env.PORT || 5001
+const PORT = process.env.MYSQLPORT || 5001;
 dotenv.config();
-app.use(express.json())
-app.use(cors({
-    origin: 'http://localhost:5173',
-  }));
-4
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+4;
 //routes
 app.use("/api/guests", require("./routes/seatingRoute"));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
