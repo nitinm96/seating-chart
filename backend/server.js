4e57f543197517b6664da0d4cea2504405b24c6b
Nitin Minhas <nitin.minhas@hotmail.com>
2025-06-22 02:44:34 -0400

    fix cors for backend server

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://nlfindyourseat.netlify.app/"],
   })
 );
 4;
